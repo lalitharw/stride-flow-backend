d30e608b2072b1d54878c2525412a8e213549b05
lalitharw <lalitharwate90@gmail.com>
2026-06-22 23:06:54 +0530

    updated

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return response([
-        "message" => "Api Up and Running!",
+        "message" => "Api Up and Running Successfully!",
         "success" => true
     ], 200);
 });
